7d9e0e712a106dbd0e6e8b3a686ce7d903d0737d
Saianeesh Keshav Haridas <saianeesh@protonmail.com>
2019-11-25 21:41:33 -0500

    Added support for longer keys

--- client/send.py
+++ client/send.py
@@ -33,11 +33,14 @@ for line in f.readlines():
     key_body = line[2:]
 
     op_field = struct.pack("B", NC_READ_REQUEST)
+    fake_field = ""
+    for i in range(16):
+        fake_field += struct.pack("B", 0)
     key_field = struct.pack(">I", key_header)
     for i in range(len(key_body)):
         key_field += struct.pack("B", int(key_body[i], 16))
-    packet = op_field + key_field
-    
+    packet = op_field + fake_field + key_field
+
     s.sendto(packet, (SERVER_IP, NC_PORT))
     counter = counter + 1
     time.sleep(interval)
